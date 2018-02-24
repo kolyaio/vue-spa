1220e76a1a9c0b2b6b4252eaefdec1b4e6ad814f
Nikolai <nick2ilk@gmail.com>
2018-02-24 15:08:08 +0200

    error in deployment script

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -4,11 +4,11 @@ call npm run build
 call cd %DEPLOYMENT_TARGET%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
-xcopy /d %DEPLOYMENT_TARGET%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
-xcopy /d %DEPLOYMENT_TARGET%\index.html %DEPLOYMENT_TARGET%\index.html*
-xcopy /d %DEPLOYMENT_TARGET%\server.js %DEPLOYMENT_TARGET%\server.js*
-xcopy /d %DEPLOYMENT_TARGET%\package.json %DEPLOYMENT_TARGET%\package.json*
-xcopy /d %DEPLOYMENT_TARGET%\build\web.config %DEPLOYMENT_TARGET%\web.config*
+xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
+xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
+xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
+xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config*
 
 call npm install --only=production
 echo Deployed.
